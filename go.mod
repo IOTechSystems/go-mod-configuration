2153f9668d59e1101ef4af7a65914f666e08694e
Lindsey Cheng <beckysocute@gmail.com>
2022-07-05 23:17:10 +0800

    PS-335 Add the core keeper configuration provider (#2)
    * Initial commit for adding core keeper client
    
    Initial commit for adding core keeper config client.
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    * PS-335 Add PutConfiguration related methods
    
    Add PutConfiguration related methods to keeper client.
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    * PS-335 Add the decode func for GetConfiguration
    
    Add decode func for GetConfiguration.
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    * PS-335 Update go.mod
    
    Update go.mod file.
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    * PS-335 Adjust DTOs and file structure
    
    Remove unused DTO defs and adjust code file structure.
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    * PS-335 Rename api client and improve config data type handling
    
    Rename api client and improve config data type handling.
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/edgexfoundry/go-mod-configuration/v2
 require (
 	github.com/hashicorp/consul/api v1.12.0
 	github.com/mitchellh/consulstructure v0.0.0-20190329231841-56fdc4d2da54
+	github.com/mitchellh/mapstructure v1.1.2
 	github.com/pelletier/go-toml v1.9.5
 	github.com/stretchr/testify v1.7.1
 )
@@ -15,18 +16,23 @@ require (
 	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
 	github.com/hashicorp/go-hclog v0.12.0 // indirect
 	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.0 // indirect
 	github.com/hashicorp/serf v0.9.6 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.6 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/mitchellh/copystructure v1.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
-	github.com/mitchellh/mapstructure v1.1.2 // indirect
 	github.com/mitchellh/reflectwalk v1.0.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	github.com/stretchr/objx v0.1.1 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
 
 go 1.17
---
+++ internal/pkg/keeper/client.go
@@ -0,0 +1,210 @@
+//
+// Copyright (C) 2022 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package keeper
+
+import (
+	"context"
+	"fmt"
+	"path"
+	"strconv"
+	"sync"
+
+	"github.com/edgexfoundry/go-mod-configuration/v2/internal/pkg/keeper/api"
+	"github.com/edgexfoundry/go-mod-configuration/v2/pkg/types"
+
+	"github.com/pelletier/go-toml"
+)
+
+type keeperClient struct {
+	keeperUrl       string
+	keeperClient    *api.Caller
+	configBasePath  string
+	watchingDoneCtx context.Context
+	watchingDone    context.CancelFunc
+	watchingWait    sync.WaitGroup
+}
+
+// NewKeeperClient creates a new Keeper Client.
+func NewKeeperClient(config types.ServiceConfig) *keeperClient {
+	client := keeperClient{
+		keeperUrl:      config.GetUrl(),
+		configBasePath: config.BasePath,
+	}
+
+	client.watchingDoneCtx, client.watchingDone = context.WithCancel(context.Background())
+
+	client.createKeeperClient(client.keeperUrl)
+	return &client
+}
+
+func (client *keeperClient) fullPath(name string) string {
+	return path.Join(client.configBasePath, name)
+}
+
+func (client *keeperClient) createKeeperClient(url string) {
+	client.keeperClient = api.NewCaller(url)
+}
+
+// IsAlive simply checks if Core Keeper is up and running at the configured URL
+func (client *keeperClient) IsAlive() bool {
+	err := client.keeperClient.Ping()
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+// HasConfiguration checks to see if Consul contains the service's configuration.
+func (client *keeperClient) HasConfiguration() (bool, error) {
+	resp, err := client.keeperClient.KV().Keys(client.configBasePath)
+	if err != nil {
+		return false, fmt.Errorf("checking configuration existence from Core Keeper failed: %v", err)
+	}
+	if len(resp.Keys) == 0 {
+		return false, nil
+	}
+	return true, nil
+}
+
+func (client *keeperClient) HasSubConfiguration(name string) (bool, error) {
+	keyPath := client.fullPath(name)
+	resp, err := client.keeperClient.KV().Keys(keyPath)
+	if err != nil {
+		return false, fmt.Errorf("checking configuration existence from Core Keeper failed: %v", err)
+	}
+	if len(resp.Keys) == 0 {
+		return false, nil
+	}
+	return true, nil
+}
+
+// PutConfigurationToml puts a full toml configuration into Core Keeper
+func (client *keeperClient) PutConfigurationToml(configuration *toml.Tree, overwrite bool) error {
+	configurationMap := configuration.ToMap()
+	err := client.PutConfiguration(configurationMap, overwrite)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (client *keeperClient) PutConfiguration(config interface{}, overwrite bool) error {
+	var err error
+	if overwrite {
+		err = client.keeperClient.KV().PutKeys(client.configBasePath, config)
+	} else {
+		kvPairs := convertMapToKVPairs("", config)
+		for _, kv := range kvPairs {
+			exists, err := client.ConfigurationValueExists(kv.Key)
+			if err != nil {
+				return err
+			}
+			if !exists {
+				// Only create the key if not exists in core keeper
+				if err = client.PutConfigurationValue(kv.Key, []byte(kv.Value)); err != nil {
+					return err
+				}
+			}
+		}
+	}
+	if err != nil {
+		return fmt.Errorf("error occurred while creating/updating configuration, error: %v", err)
+	}
+	return nil
+}
+
+func (client *keeperClient) GetConfiguration(configStruct interface{}) (interface{}, error) {
+	exists, err := client.HasConfiguration()
+	if err != nil {
+		return nil, err
+	}
+
+	if !exists {
+		return nil, fmt.Errorf("the Configuration service (EdgeX Keeper) doesn't contain configuration for %s", client.configBasePath)
+	}
+
+	resp, err := client.keeperClient.KV().Get(client.configBasePath)
+	if err != nil {
+		return nil, err
+	}
+
+	err = decode(client.configBasePath+api.KeyDelimiter, resp.KVs, configStruct)
+	if err != nil {
+		return nil, err
+	}
+	return configStruct, nil
+}
+
+func (client *keeperClient) WatchForChanges(updateChannel chan<- interface{}, errorChannel chan<- error, configuration interface{}, waitKey string) {
+}
+
+func (client *keeperClient) StopWatching() {}
+
+func (client *keeperClient) ConfigurationValueExists(name string) (bool, error) {
+	keyPath := client.fullPath(name)
+	res, err := client.keeperClient.KV().Keys(keyPath)
+	if err != nil {
+		return false, fmt.Errorf("checking configuration existence from Core Keeper failed: %v", err)
+	}
+	if len(res.Keys) == 0 {
+		return false, nil
+	}
+	return true, nil
+}
+
+func (client *keeperClient) GetConfigurationValue(name string) ([]byte, error) {
+	keyPath := client.fullPath(name)
+	resp, err := client.keeperClient.KV().Get(keyPath)
+	if err != nil {
+		return nil, err
+	}
+	if len(resp.KVs) == 0 {
+		return nil, fmt.Errorf("%s configuration not found", name)
+	}
+
+	var valueStr string
+	switch value := resp.KVs[0].Value.(type) {
+	case string:
+		valueStr = value
+	case int:
+		valueStr = strconv.Itoa(value)
+	case int8:
+		value8 := int(value)
+		valueStr = strconv.Itoa(value8)
+	case int16:
+		value16 := int(value)
+		valueStr = strconv.Itoa(value16)
+	case int32:
+		value32 := int(value)
+		valueStr = strconv.Itoa(value32)
+	case int64:
+		value64 := int(value)
+		valueStr = strconv.Itoa(value64)
+	case float32:
+		valueF64 := float64(value)
+		valueStr = strconv.FormatFloat(valueF64, 'g', -1, 32)
+	case float64:
+		valueStr = strconv.FormatFloat(value, 'g', -1, 64)
+	case bool:
+		valueStr = strconv.FormatBool(value)
+	case nil:
+		valueStr = ""
+	default:
+		valueStr = fmt.Sprintf("%v", value)
+	}
+
+	return []byte(valueStr), nil
+}
+
+func (client *keeperClient) PutConfigurationValue(name string, value []byte) error {
+	keyPath := client.fullPath(name)
+	err := client.keeperClient.KV().Put(keyPath, value)
+	if err != nil {
+		return fmt.Errorf("unable to JSON marshal configStruct, err: %v", err)
+	}
+	return nil
+}
